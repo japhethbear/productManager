9e829fbc1082e469ed3a33e51f8428fa28d0ae25
japhethbear <japhethbear@gmail.com>
2023-03-14 10:38:32 -0500

    first commit

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,17 @@
+const Product = require('../models/product.model');
+
+
+// What is this below??
+
+module.exports.index = (req, res) => {
+    res.json({
+        message: "Hello World"
+    })
+}
+
+module.exports.createProduct = (req, res) => {
+    Product.create(req.body)
+        .then(product => res.json(product))
+        .catch(err => res.json(err));
+}
+
